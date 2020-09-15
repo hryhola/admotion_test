e562599a0f03d2e94a60646eb0cf905551ee2265
VladGrygola <vladgrygola@gmail.com>
2020-09-15 17:15:13 +0300

    Updates

---
+++ precache-manifest.2a61a80d6159ce646805b35864a834cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcd48ad7e3eafc776ee3ffb369e42de",
+    "url": "/admotion_test/index.html"
+  },
+  {
+    "revision": "8c2cb240e7479c43ba31",
+    "url": "/admotion_test/static/css/main.db0c265d.chunk.css"
+  },
+  {
+    "revision": "5ff021448490aaf1c9f6",
+    "url": "/admotion_test/static/js/2.20892140.chunk.js"
+  },
+  {
+    "revision": "91e9bfd1d08880b66e0e6b773fb8e781",
+    "url": "/admotion_test/static/js/2.20892140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2cb240e7479c43ba31",
+    "url": "/admotion_test/static/js/main.59af3bc1.chunk.js"
+  },
+  {
+    "revision": "369e3c9c3d3bf00d5a9d",
+    "url": "/admotion_test/static/js/runtime-main.d999a458.js"
+  }
+]);
